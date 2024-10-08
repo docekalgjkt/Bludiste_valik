68110696f1a2a477ff8110dee802ae72f510953b
Matej Valik <valik.m@post.cz>
2024-10-08 14:33:21 +0200

    cheese

---
+++ kod.py
@@ -0,0 +1,20 @@
+import tkinter as tk
+class Bludiste:
+    def __init__(self, canvas):
+
+canvas = tk.Canvas(canvas, width=500, height=500)
+
+def draw_bludiste:
+    for i in bludiste:
+        if i = 1:
+            pole = canvas.create_rectangle()
+
+
+bludiste = ([[1,0,1,0],
+             [1,0,1,0],
+             [1,0,0,0],
+             [1,1,1,1],
+             ])
+
+canvas.pack()
+root.mainloop()
