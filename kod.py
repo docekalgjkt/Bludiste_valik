7d9aadb8848887756f2d3a921c8c0a2364ca6475
Matej Valik <valik.m@post.cz>
2024-10-15 14:34:07 +0200

    Bludiste, BludisteApp

--- kod.py
+++ kod.py
@@ -1,4 +1,24 @@
 import tkinter as tk
+from Tools.scripts.mailerdaemon import emparse_list_from
+
+
+class BludisteApp(tk.Tk):
+    def __init__(self):
+        tk.Tk.__init__(self)
+
+
+
+class Bludiste:
+    def __init__(self, bludiste_data):
+        self.bludiste_data = bludiste_data
+
+    def getSirka(self):
+        sirka = len(self.bludiste_data)
+        return sirka
+
+    def getVyska(self):
+        vyska = len(self.bludiste_data[0])
+        return vyska
 
 
 class BludisteView:
@@ -20,7 +40,6 @@ class BludisteView:
                     color = 'black' if self.bludiste_data[i][j] == 1 else 'white'
                     self.canvas.create_rectangle(x1, y1, x2, y2, fill=color, outline="gray")
 
-
 root = tk.Tk()
 canvas = tk.Canvas(root, width=500, height=500)
 canvas.pack()
@@ -33,5 +52,13 @@ bludiste_data = ([[1, 0, 1, 0],
 
 bludiste = BludisteView(canvas, bludiste_data, 100)
 bludiste.kresli_bludiste()
+
+bludiste_objekt = Bludiste(bludiste_data)
+
+sirka = bludiste_objekt.getSirka()
+print("sirka =", sirka)
+
+vyska = bludiste_objekt.getVyska()
+print("vyska =", vyska)
 
 root.mainloop()
