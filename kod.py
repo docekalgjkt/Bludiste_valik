48cc4196cdc97805b024889bae39070666d865ce
Matej Valik <valik.m@post.cz>
2024-11-12 14:30:35 +0100

    save_to_file

--- kod.py
+++ kod.py
@@ -22,6 +22,12 @@ class Bludiste:
         vyska = len(self.bludiste_data[0])
         return vyska
 
+    def save_to_file(self, filename):
+        with open(filename, 'w') as file:
+            for row in self.bludiste_data:
+                file.write(' '.join(map(str, row)) + '\n')
+        print(f"Bludiste data saved to {filename}")
+
 
 class DesignPattern:
     def __init__(self):
@@ -33,17 +39,10 @@ class DesignPattern:
 class BludisteView:
     def __init__(self, canvas, bludiste_data, size):
         self.canvas = canvas
+        self.bludiste_data = bludiste_data
         self.size = size
 
-    def create_bludiste(self):
-        n = self.size
-        self.bludiste_data = np.random.randint(2, size=(n, n))
-        print(self.bludiste_data)
-
     def kresli_bludiste(self):
-        if self.bludiste_data is None:
-            raise ValueError("Bludiste data nebyla vytvořena. Spusťte nejdříve metodu 'create_bludiste'.")
-
             radky = len(self.bludiste_data)
             sloupce = len(self.bludiste_data[0])
 
@@ -56,22 +55,28 @@ class BludisteView:
                     color = 'black' if self.bludiste_data[i][j] == 1 else 'white'
                     self.canvas.create_rectangle(x1, y1, x2, y2, fill=color, outline="gray")
 
+
+bludiste_data = ([[1, 0, 1, 0],
+                [1, 0, 1, 0],
+                [1, 0, 0, 0],
+                [0, 1, 1, 1],
+            ])
+
+
 root = tk.Tk()
 canvas = tk.Canvas(root, width=500, height=500)
 canvas.pack()
-
-bludiste = BludisteView(canvas, bludiste_data, size=100)
-bludiste.create_bludiste()
 
 bludiste = BludisteView(canvas, bludiste_data, 100)
 bludiste.kresli_bludiste()
 
 bludiste_objekt = Bludiste(bludiste_data)
-
 sirka = bludiste_objekt.getSirka()
 print("sirka =", sirka)
-
 vyska = bludiste_objekt.getVyska()
 print("vyska =", vyska)
+
+bludiste_objekt.save_to_file("bludiste_data.txt")
+
 
 root.mainloop()
